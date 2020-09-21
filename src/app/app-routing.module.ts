92de0a650720fc85d939c48dae0fa49814b43d58
f4 <straka.filip@gmail.com>
2020-09-21 02:25:17 +0200

    Add ListComponent And Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListComponent} from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/list',
+    component: ListComponent
+  },
+  {
+    path: '/auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/list'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
